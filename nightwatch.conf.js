2b03976725535b2e557e40dc0c9bc188673e9fe8
loic-bodolec <loic.bodolec@basikon.io>
2022-01-28 11:26:46 +0100

    docs: comments in conig file

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,7 +1,12 @@
 module.exports = {
+    // An array of folders (excluding subfolders) where your tests are located;
+    // if this is not specified, the test source must be passed as the second argument to the test runner.
     src_folders: ["tests"],
+    // Running the remaining test steps/test cases is possible
     skip_testcases_on_fail: false,
+    // See https://nightwatchjs.org/guide/working-with-page-objects/
     page_objects_path: "pageObjects",
+    // See https://nightwatchjs.org/guide/extending-nightwatch/#writing-custom-commands
     custom_commands_path: "./commands",
     screenshots: {
         enabled: true,
